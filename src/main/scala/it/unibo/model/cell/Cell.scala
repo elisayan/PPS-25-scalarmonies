b3f8027b4a23db64526253a444cd9ea24897fa5e
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2026-06-25 17:50:17 +0200

    refactored the cell implementation and scala test

--- src/main/scala/it/unibo/model/cell/Cell.scala
+++ src/main/scala/it/unibo/model/cell/Cell.scala
@@ -1,12 +1,12 @@
 package it.unibo.model.cell
 
-class Cell(private val tokens: List[Any]):
+case class Cell(private val tokens: List[Any] = List()):
 
   def hasTokens: Boolean = tokens.nonEmpty
 
   def getTokens: List[Any] = tokens
 
   def placeToken(token: Any): Cell =
-    val list = List(token)
-    val cell = new Cell(tokens ::: list)
-    cell
+    Cell(tokens :+ token)
+
+  def topToken: Option[Any] = tokens.lastOption
--- src/test/scala/it/unibo/model/cell/CellTest.scala
+++ src/test/scala/it/unibo/model/cell/CellTest.scala
@@ -1,7 +1,6 @@
 package it.unibo.model.cell
 
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers
 
 import scala.language.postfixOps
@@ -10,29 +9,48 @@ class CellTest extends AnyFlatSpec with Matchers:
 
   case class Token(color: String)
 
-  "A Cell" should "be empty when initialized"
-  val cell = new Cell(List())
-  cell.getTokens should have size 0
-  cell.hasTokens should be (false)
+  "A Cell" should "be empty when initialized" in {
+    val cell = Cell(List())
+    cell.getTokens should have size 0
+    cell.hasTokens should be(false)
+    cell.topToken shouldBe None
+  }
 
-  it should "return a new Cell when a Token is placed"
-  val token = Token("Red")
-  val result: Cell = cell.placeToken(token)
-  result shouldBe a [Cell]
-  result.hasTokens should be (true)
-  result.getTokens shouldBe a [List[_]]
+  it should "return a new Cell when a Token is placed" in {
+    val cell = Cell(List())
+    val token = Token("Red")
+    val result: Cell = cell.placeToken(token)
 
-  it should "return the placed token(s)"
-  result.getTokens should not be empty
-  result.getTokens shouldBe a [List[_]]
-  result.getTokens.head should be (Token("Red"))
+    result shouldBe a[Cell]
+    result.hasTokens should be(true)
+    result.getTokens shouldBe a[List[_]]
 
-  it should "preserve the right order of the tokens placed inside of it"
-  val cell2 = new Cell(List(Token("Red")))
-  val token2 = Token("Grey")
-  val token3 = Token("Green")
-  val res1: Cell = cell2.placeToken(token2)
-  val res2: Cell = res1.placeToken(token3)
-  res2.getTokens should have size 3
-  res2.getTokens should contain theSameElementsInOrderAs List(Token("Red"), Token("Grey"), Token("Green"))
+    cell.hasTokens should be(false)
+  }
 
+  it should "return the placed token(s)" in {
+    val cell = Cell(List())
+    val token = Token("Red")
+    val result: Cell = cell.placeToken(token)
+
+    result.getTokens should not be empty
+    result.getTokens.head shouldBe Token("Red")
+  }
+
+  it should "preserve the right order of the tokens placed inside of it" in {
+    val cell2 = Cell(List(Token("Red")))
+    val token2 = Token("Grey")
+    val token3 = Token("Green")
+
+    val res1: Cell = cell2.placeToken(token2)
+    val res2: Cell = res1.placeToken(token3)
+
+    res2.getTokens should have size 3
+
+    res2.getTokens should contain theSameElementsInOrderAs List(
+      Token("Red"),
+      Token("Grey"),
+      Token("Green")
+    )
+    res2.topToken shouldBe Some(Token("Green"))
+  }
