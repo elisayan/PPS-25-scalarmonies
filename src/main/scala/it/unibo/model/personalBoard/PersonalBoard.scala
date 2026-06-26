296f306e4eb1dde52b05b60d01d474f576a8ee83
Oluwatobi Daniel Ariyo - oluwatobi.ariyo@studio.unibo.it <oluwatobi.ariyo@studio.unibo.it>
2026-06-26 17:09:11 +0200

    mid way through implementing personal board

--- src/main/scala/it/unibo/model/personalBoard/PersonalBoard.scala
+++ src/main/scala/it/unibo/model/personalBoard/PersonalBoard.scala
@@ -1,3 +1,64 @@
 package it.unibo.model.personalBoard
 
-class PersonalBoard {}
+import it.unibo.model.cell.Cell
+
+case class Token(color: String)
+
+trait PersonalBoard:
+  def heightBound: Int
+  def widthBound: Int
+  def cells: Map[Coordinate, Cell]
+
+  def getNorthernNeighbour(coordinate: Coordinate): Option[Cell]
+  def getSouthernNeighbour(coordinate: Coordinate): Option[Cell]
+  def getSouthEasternNeighbour(coordinate: Coordinate): Option[Cell]
+  def getSouthWesternNeighbour(coordinate: Coordinate): Option[Cell]
+  def getNortEasternNeighbour(coordinate: Coordinate): Option[Cell]
+  def getNortWesternNeighbour(coordinate: Coordinate): Option[Cell]
+  def placeToken(token: Token): PersonalBoard
+
+object PersonalBoard:
+
+  def apply(
+      heightBound: Int,
+      widthBound: Int,
+      cells: Map[Coordinate, Cell]
+  ): PersonalBoard = PersonalBoardImpl(heightBound, widthBound, cells)
+
+  def unapply(board: PersonalBoard): Option[(Int, Int, Map[Coordinate, Cell])] =
+    if board == null then None
+    else Some((board.heightBound, board.widthBound, board.cells))
+
+  private case class PersonalBoardImpl(
+      override val heightBound: Int,
+      override val widthBound: Int,
+      override val cells: Map[Coordinate, Cell]
+  ) extends PersonalBoard:
+
+    private def isValid(c: Coordinate): Boolean =
+      c.x.abs <= widthBound && c.y.abs <= heightBound
+
+    private def checkBounds(c: Coordinate): Option[Cell] =
+      if isValid(c) then cells.get(c) else None
+
+    override def getNorthernNeighbour(coordinate: Coordinate): Option[Cell] =
+      ???
+
+    override def getSouthernNeighbour(coordinate: Coordinate): Option[Cell] =
+      ???
+
+    override def getSouthEasternNeighbour(
+        coordinate: Coordinate
+    ): Option[Cell] = ???
+
+    override def getSouthWesternNeighbour(
+        coordinate: Coordinate
+    ): Option[Cell] = ???
+
+    override def getNortEasternNeighbour(coordinate: Coordinate): Option[Cell] =
+      ???
+
+    override def getNortWesternNeighbour(coordinate: Coordinate): Option[Cell] =
+      ???
+
+    override def placeToken(token: Token): PersonalBoard = ???
--- src/test/scala/it/unibo/model/personalBoard/PersonalBoardTest.scala
+++ src/test/scala/it/unibo/model/personalBoard/PersonalBoardTest.scala
@@ -1,40 +1,35 @@
 package it.unibo.model.personalBoard
 
+import it.unibo.model.cell.Cell
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class PersonalBoardTest extends AnyFlatSpec with Matchers:
 
-  val board = PersonalBoard(4, 4, 23)
+  val map: Map[Coordinate, Cell] = Map()
+  val board = PersonalBoard(4, 4, map)
   val westBorder = Coordinate(-4, 0)
   val eastBorder = Coordinate(4, 0)
   val southBorder = Coordinate(0, -4)
   val northBorder = Coordinate(0, 4)
 
   "A Personal Board" should "have a fixed size and fixed bounds" in:
-    board should matchPattern { case PersonalBoard(4, 4, 23) => }
+    board should matchPattern { case PersonalBoard(4, 4, map) => }
 
   it should "return None when requesting a neighbor beyond the western boundary" in:
-    board.getNorthWestNeighbour(westBorder) shouldBe None
-    board.getSouthWestNeighbour(westBorder) shouldBe None
-
-    board.getNorthEastNeighbour(westBorder) should matchPattern { case Some(Cell(-2, 1)) => }
-    board.getSouthEastNeighbour(westBorder) should matchPattern { case Some(Cell(-2, -1)) => }
+    board.getNortWesternNeighbour(westBorder) shouldBe None
+    board.getSouthWesternNeighbour(westBorder) shouldBe None
 
   it should "return None when requesting a neighbor beyond the eastern boundary" in:
-    board.getNorthEastNeighbour(eastBorder) shouldBe None
-    board.getSouthEastNeighbour(eastBorder) shouldBe None
-    board.getNorthWestNeighbour(eastBorder) should matchPattern { case Some(Cell(2, 1)) => }
-    board.getSouthWestNeighbour(eastBorder) should matchPattern { case Some(Cell(2, -1)) => }
+    board.getNortEasternNeighbour(eastBorder) shouldBe None
+    board.getSouthEasternNeighbour(eastBorder) shouldBe None
 
   it should "return None when requesting a neighbor beyond the southern boundary" in:
-    board.getSouthNeighbour(southBorder) shouldBe None
-    board.getSouthWestNeighbour(southBorder) shouldBe None
-    board.getSouthEastNeighbour(southBorder) shouldBe None
-    board.getNorthNeighbour(southBorder) should matchPattern { case Some(Cell(0, -2)) => }
+    board.getSouthernNeighbour(southBorder) shouldBe None
+    board.getSouthWesternNeighbour(southBorder) shouldBe None
+    board.getSouthEasternNeighbour(southBorder) shouldBe None
 
   it should "return None when requesting a neighbor beyond the northern boundary" in:
-    board.getNorthNeighbour(northBorder) shouldBe None
-    board.getNorthWestNeighbour(northBorder) shouldBe None
-    board.getNorthEastNeighbour(northBorder) shouldBe None
-    board.getSouthNeighbour() should matchPattern { case Some(Cell(0, 2)) => }
+    board.getNorthernNeighbour(northBorder) shouldBe None
+    board.getNortWesternNeighbour(northBorder) shouldBe None
+    board.getNortEasternNeighbour(northBorder) shouldBe None
