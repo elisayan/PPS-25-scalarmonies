4bd2f9131cc6eebd209d1cfb3549be868a6f472a
Elisa Yan <elisa.yan@studio.unibo.it>
2026-06-26 22:56:04 +0200

    refactor token

--- src/main/scala/it/unibo/model/token/Token.scala
+++ src/main/scala/it/unibo/model/token/Token.scala
@@ -3,10 +3,4 @@ package it.unibo.model.token
 enum TokenColor:
   case Grey, Brown, Green, Yellow, Blue, Red
 
-enum TerrainToken(val color: TokenColor):
-  case Mountain extends TerrainToken(TokenColor.Grey)
-  case TreeTrunk extends TerrainToken(TokenColor.Brown)
-  case TreeFoliage extends TerrainToken(TokenColor.Green)
-  case Field extends TerrainToken(TokenColor.Yellow)
-  case Water extends TerrainToken(TokenColor.Blue)
-  case Building extends TerrainToken(TokenColor.Red)
+case class Token(color: TokenColor)
--- src/test/scala/it/unibo/model/token/TokenTest.scala
+++ src/test/scala/it/unibo/model/token/TokenTest.scala
@@ -5,26 +5,17 @@ import org.scalatest.matchers.should.Matchers
 
 class TokenTest extends AnyFlatSpec with Matchers:
 
-  "Mountain" should "have grey color" in {
-    TerrainToken.Mountain.color shouldBe TokenColor.Grey
-  }
+  val colors = Seq(
+    TokenColor.Grey,
+    TokenColor.Brown,
+    TokenColor.Green,
+    TokenColor.Yellow,
+    TokenColor.Blue,
+    TokenColor.Red
+  )
 
-  "TreeTrunk" should "have brown color" in {
-    TerrainToken.TreeTrunk.color shouldBe TokenColor.Brown
-  }
-
-  "TreeFoliage" should "have green color" in {
-    TerrainToken.TreeFoliage.color shouldBe TokenColor.Green
-  }
-
-  "Field" should "have yellow color" in {
-    TerrainToken.Field.color shouldBe TokenColor.Yellow
-  }
-
-  "Water" should "have blue color" in {
-    TerrainToken.Water.color shouldBe TokenColor.Blue
-  }
-
-  "Building" should "have red color" in {
-    TerrainToken.Building.color shouldBe TokenColor.Red
+  "A Token" should "store the provided color" in {
+    colors.foreach { color =>
+      Token(color).color shouldBe color
+    }
   }
