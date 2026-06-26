015abc14a51b0b0d791e53a701b9a5607a270578
Oluwatobi Daniel Ariyo - oluwatobi.ariyo@studio.unibo.it <oluwatobi.ariyo@studio.unibo.it>
2026-06-26 16:02:47 +0200

    implemented the test class for personal board, now is red because the actual implementation of personal board still needs to be done

--- src/test/scala/it/unibo/model/personalBoard/PersonalBoardTest.scala
+++ src/test/scala/it/unibo/model/personalBoard/PersonalBoardTest.scala
@@ -5,4 +5,36 @@ import org.scalatest.matchers.should.Matchers
 
 class PersonalBoardTest extends AnyFlatSpec with Matchers:
 
-  "A Personal Board" should "have a fixed size"
+  val board = PersonalBoard(4, 4, 23)
+  val westBorder = Coordinate(-4, 0)
+  val eastBorder = Coordinate(4, 0)
+  val southBorder = Coordinate(0, -4)
+  val northBorder = Coordinate(0, 4)
+
+  "A Personal Board" should "have a fixed size and fixed bounds" in:
+    board should matchPattern { case PersonalBoard(4, 4, 23) => }
+
+  it should "return None when requesting a neighbor beyond the western boundary" in:
+    board.getNorthWestNeighbour(westBorder) shouldBe None
+    board.getSouthWestNeighbour(westBorder) shouldBe None
+
+    board.getNorthEastNeighbour(westBorder) should matchPattern { case Some(Cell(-2, 1)) => }
+    board.getSouthEastNeighbour(westBorder) should matchPattern { case Some(Cell(-2, -1)) => }
+
+  it should "return None when requesting a neighbor beyond the eastern boundary" in:
+    board.getNorthEastNeighbour(eastBorder) shouldBe None
+    board.getSouthEastNeighbour(eastBorder) shouldBe None
+    board.getNorthWestNeighbour(eastBorder) should matchPattern { case Some(Cell(2, 1)) => }
+    board.getSouthWestNeighbour(eastBorder) should matchPattern { case Some(Cell(2, -1)) => }
+
+  it should "return None when requesting a neighbor beyond the southern boundary" in:
+    board.getSouthNeighbour(southBorder) shouldBe None
+    board.getSouthWestNeighbour(southBorder) shouldBe None
+    board.getSouthEastNeighbour(southBorder) shouldBe None
+    board.getNorthNeighbour(southBorder) should matchPattern { case Some(Cell(0, -2)) => }
+
+  it should "return None when requesting a neighbor beyond the northern boundary" in:
+    board.getNorthNeighbour(northBorder) shouldBe None
+    board.getNorthWestNeighbour(northBorder) shouldBe None
+    board.getNorthEastNeighbour(northBorder) shouldBe None
+    board.getSouthNeighbour() should matchPattern { case Some(Cell(0, 2)) => }
