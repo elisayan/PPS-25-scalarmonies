9fe26822c795c1afc6bcf30a62733fbcf06288c0
Oluwatobi Daniel Ariyo - oluwatobi.ariyo@studio.unibo.it <oluwatobi.ariyo@studio.unibo.it>
2026-06-26 12:15:06 +0200

    created the entity Personal Board and its test class

---
+++ src/main/scala/it/unibo/model/personalBoard/PersonalBoard.scala
@@ -0,0 +1,5 @@
+package it.unibo.model.personalBoard
+
+class PersonalBoard {
+
+}
---
+++ src/test/scala/it/unibo/model/personalBoard/PersonalBoardTest.scala
@@ -0,0 +1,7 @@
+package it.unibo.model.personalBoard
+
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class PersonalBoardTest extends AnyFunSuiteLike {
+
+}
