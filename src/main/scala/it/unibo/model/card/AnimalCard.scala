d5a7029259fcafa8b60f9ae0babc6daaf82083db
Filoferro03 <filippo.ferretti03@gmail.com>
2026-07-04 18:24:25 +0200

    Added placeCube function to place animalCubes

--- src/main/scala/it/unibo/model/card/AnimalCard.scala
+++ src/main/scala/it/unibo/model/card/AnimalCard.scala
@@ -17,22 +17,21 @@ trait AnimalCard extends Card:
   def name: String
   def habitat: Habitat
   def points: List[Int]
-  def cubesRemaining: Int
   def maxCubes: Int
   def placedCubes: Int
   def currentPoints: Int
+  def placeCube: Option[AnimalCard]
 
 object AnimalCard:
   def apply(name: String, habitat: Habitat, points: List[Int]): AnimalCard =
-    AnimalCardImpl(name, habitat, points)
+    AnimalCardImpl(name, habitat, points, points.length)
 
   private case class AnimalCardImpl(
                                      override val name: String,
                                      override val habitat: Habitat,
                                      override val points: List[Int],
+                                     cubesRemaining: Int
                                    ) extends AnimalCard:
-
-    override val cubesRemaining: Int = points.size
 
     override val maxCubes: Int = points.length
 
@@ -43,3 +42,7 @@ object AnimalCard:
       if placed <= 0 then 0
       else if placed > points.length then points.last
       else points(placed - 1)
+
+    override def placeCube: Option[AnimalCard] =
+      if cubesRemaining > 0 then Some(copy(cubesRemaining = cubesRemaining - 1))
+      else None
--- src/test/scala/it/unibo/model/card/AnimalCardTest.scala
+++ src/test/scala/it/unibo/model/card/AnimalCardTest.scala
@@ -17,3 +17,14 @@ class AnimalCardTest extends AnyFunSuite:
     card.maxCubes shouldBe 3
     card.placedCubes shouldBe 0
     card.currentPoints shouldBe 0
+
+
+  test("Piazzare un cubo deve restituire una nuova istanza e aggiornare i punti"):
+    val initialCard = AnimalCard("Orso", habitat, testPoints)
+    val card1 = initialCard.placeCube
+    card1.isDefined shouldBe true
+    card1.get.placedCubes shouldBe 1
+    card1.get.currentPoints shouldBe 4
+    val card2 = card1.get.placeCube
+    card2.get.placedCubes shouldBe 2
+    card2.get.currentPoints shouldBe 9
