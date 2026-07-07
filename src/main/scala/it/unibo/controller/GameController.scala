508f485963a135ea4c01c298022130f9d98a9830
Elisa Yan <elisa.yan@studio.unibo.it>
2026-07-07 20:03:42 +0200

    initialize GameController

--- src/main/scala/it/unibo/controller/GameController.scala
+++ src/main/scala/it/unibo/controller/GameController.scala
@@ -1,3 +1,33 @@
 package it.unibo.controller
 
-class GameController {}
+import it.unibo.model.GameModel
+import it.unibo.model.personalBoard.Coordinate
+import it.unibo.view.GameView
+
+trait GameController:
+  def currentModel: GameModel
+  def onTakeTokens(slot: Int): Unit
+  def onPlaceToken(coordinate: Coordinate): Unit
+  def onEndTurn(): Unit
+
+object GameController:
+
+  def apply(model: GameModel): GameController =
+    GameControllerImpl(model)
+
+  private class GameControllerImpl(private var model: GameModel) extends GameController:
+
+    override def currentModel: GameModel = model
+
+    override def onTakeTokens(slot: Int): Unit =
+      //try
+        model = model.takeTokens(slot)
+      //  view.render(model)
+      //catch
+        //case e: IllegalStateException => view.showError(e.getMessage)
+
+    override def onPlaceToken(coordinate: Coordinate): Unit =
+      model = model.placeToken(coordinate)
+
+    override def onEndTurn(): Unit =
+      model = model.endTurn()
