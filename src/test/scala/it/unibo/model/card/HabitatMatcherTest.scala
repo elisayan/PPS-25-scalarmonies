24e5525d898d412c99525d0c5bfe1d70b942d271
Filoferro03 <filippo.ferretti03@gmail.com>
2026-07-08 15:37:46 +0200

    Modified test for changing habitatMatcher

--- src/test/scala/it/unibo/model/card/HabitatMatcherTest.scala
+++ src/test/scala/it/unibo/model/card/HabitatMatcherTest.scala
@@ -7,33 +7,51 @@ import it.unibo.model.cell.Cell
 import it.unibo.model.token.TerrainToken
 
 class HabitatMatcherTest extends AnyFunSuite:
-  private val treeCell = Cell(List(TerrainToken.Forest))
-  private val waterCell = Cell(List(TerrainToken.Water))
-  private val testCell = Map(
-    Coordinate(0, 0) -> treeCell,
-    Coordinate(0, 1) -> waterCell
-  )
-  private val testBoard = PersonalBoard(heightBound = 3, widthBound = 3, cells = testCell)
   private val testHabitat = Habitat(List(
     CellRequirement(Coordinate(0, 0), TerrainToken.Forest, 1),
-    CellRequirement(Coordinate(0, 1), TerrainToken.Water, 1)
+    CellRequirement(Coordinate(0, 2), TerrainToken.Water, 1),
   ))
+  private val treeCellFree = Cell(List(TerrainToken.Forest))
+  private val treeCellOccupied = Cell(List(TerrainToken.Forest), hasAnimal = true)
+  private val waterCell = Cell(List(TerrainToken.Water))
+  private val boardWithFreeTarget = PersonalBoard(
+    heightBound = 3,
+    widthBound = 3,
+    cells = Map(
+      Coordinate(0, 0) -> treeCellFree,
+      Coordinate(0, 2) -> waterCell
+    )
+  )
+  private val boardWithOccupiedTarget = PersonalBoard(
+    heightBound = 3,
+    widthBound = 3,
+    cells = Map(
+      Coordinate(0, 0) -> treeCellOccupied,
+      Coordinate(0, 2) -> waterCell
+    )
+  )
 
-  test("isMatch deve ritornare true se la plancia soddisfa l'habitat a partire dall'origine indicata" +
-    " altrimenti deve ritornare falso"):
-    HabitatMatcher.isMatch(testBoard, Coordinate(0, 0), testHabitat) shouldBe true
-    HabitatMatcher.isMatch(testBoard, Coordinate(0, 1), testHabitat) shouldBe false
-    HabitatMatcher.isMatch(testBoard, Coordinate(5, 5), testHabitat) shouldBe false
+  test("isMatch deve ritornare true se la plancia soddisfa l'habitat a partire dall'origine indicata altrimenti falso"):
+    HabitatMatcher.isMatch(boardWithFreeTarget, Coordinate(0, 0), testHabitat) shouldBe true
+    HabitatMatcher.isMatch(boardWithFreeTarget, Coordinate(2, 1), testHabitat) shouldBe false
+    HabitatMatcher.isMatch(boardWithFreeTarget, Coordinate(5, 5), testHabitat) shouldBe false
 
-  test("findMatches deve ritornare l'insieme esatto di tutte le origini valide sulla plancia"):
-    val matches = HabitatMatcher.findMatches(testBoard, testHabitat)
+  test("findMatches deve ritornare l'insieme esatto di HabitatMatch validi sulla plancia"):
+    val matches = HabitatMatcher.findMatches(boardWithFreeTarget, testHabitat)
     matches should have size 1
-    matches should contain(Coordinate(0, 0))
+    val foundMatch = matches.head
+    foundMatch.origin shouldBe Coordinate(0, 0)
+    foundMatch.involvedCells should have size 2
+    foundMatch.involvedCells should contain allOf (Coordinate(0, 0), Coordinate(0, 2))
 
   test("findMatches deve ritornare un Set vuoto se l'habitat non è presente da nessuna parte"):
     val impossibleHabitat = Habitat(List(
       CellRequirement(Coordinate(0, 0), TerrainToken.Field, 1),
-      CellRequirement(Coordinate(0, 1), TerrainToken.Field, 1)
+      CellRequirement(Coordinate(0, 2), TerrainToken.Field, 1)
     ))
-    val matches = HabitatMatcher.findMatches(testBoard, impossibleHabitat)
+    val matches = HabitatMatcher.findMatches(boardWithFreeTarget, impossibleHabitat)
+    matches shouldBe empty
+
+  test("findMatches deve ritornare un Set vuoto se la cella bersaglio è già occupata da un animale"):
+    val matches = HabitatMatcher.findMatches(boardWithOccupiedTarget, testHabitat)
     matches shouldBe empty
