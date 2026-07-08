3ed17d4b82e34c1729b25d942212a2fe5c7a5de4
Elisa Yan <elisa.yan@studio.unibo.it>
2026-07-08 17:28:49 +0200

    fix format

--- src/main/scala/it/unibo/view/GameView.scala
+++ src/main/scala/it/unibo/view/GameView.scala
@@ -3,21 +3,19 @@ package it.unibo.view
 import it.unibo.controller.GameController
 import scalafx.scene.layout.BorderPane
 
-
 class GameView(controller: GameController):
 
-  //private val personalBoardView = PersonalBoardView(controller)
-  //private val centralBoardView = CentralBoardView(controller)
-  //private val infoPanelView = InfoPanelView(controller)
+  // private val personalBoardView = PersonalBoardView(controller)
+  // private val centralBoardView = CentralBoardView(controller)
+  // private val infoPanelView = InfoPanelView(controller)
 
   val root = new BorderPane
-    //top = infoPanelView.root
-    //center = centralBoardView.root
-    //bottom = personalBoardView.root
+  // top = infoPanelView.root
+  // center = centralBoardView.root
+  // bottom = personalBoardView.root
 
   def refresh(): Unit =
-    //val model = controller.currentModel
-
-    //personalBoardView.refresh(model.currentPlayer.board)
-    //centralBoardView.refresh(model.centralBoard)
-    //infoPanelView.refresh(model)
+    val model = controller.currentModel
+  // personalBoardView.refresh(model.currentPlayer.board)
+  // centralBoardView.refresh(model.centralBoard)
+  // infoPanelView.refresh(model)
