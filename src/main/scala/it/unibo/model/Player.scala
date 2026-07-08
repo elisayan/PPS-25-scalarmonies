832acff19ed9a917d000891bd6313ffa398f23ca
Elisa Yan <elisa.yan@studio.unibo.it>
2026-07-09 00:35:41 +0200

    add list animal card to player

--- src/main/scala/it/unibo/model/Player.scala
+++ src/main/scala/it/unibo/model/Player.scala
@@ -1,5 +1,6 @@
 package it.unibo.model
 
+import it.unibo.model.card.AnimalCard
 import it.unibo.model.personalBoard.PersonalBoard
 
-case class Player(id: Int, board: PersonalBoard)
+case class Player(id: Int, board: PersonalBoard, activeCards: List[AnimalCard] = List())
