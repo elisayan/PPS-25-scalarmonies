192052a11353e183c3b79828742cb6a54de4dcb3
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2026-07-12 17:42:26 +0200

    finished factoring cellview, now it displays tokens inside of it if the cell field has any, now missing only the connection to game controller

--- src/main/scala/it/unibo/view/cell/CellView.scala
+++ src/main/scala/it/unibo/view/cell/CellView.scala
@@ -2,53 +2,84 @@ package it.unibo.view.cell
 
 import it.unibo.model.cell.Cell
 import it.unibo.model.personalBoard.Coordinate
+import it.unibo.model.token.TerrainToken
+import it.unibo.model.token.TerrainToken.{Forest, Ground}
+import it.unibo.view.token.TokenView
 import scalafx.scene.input.MouseEvent
 import scalafx.scene.layout.StackPane
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Polygon
 import scalafx.scene.text.Font
 import scalafx.scene.text.Text
-import scalafx.Includes._
+import scalafx.Includes.*
 
 case class CellView(
-    coordinate: Coordinate,
-    cell: Cell,
-    pos: (Double, Double) = (0.0, 0.0),
-    onCellClicked: String => Unit
-) extends StackPane:
+                     coordinate: Coordinate,
+                     var cell: Cell,
+                     pos: (Double, Double) = (0.0, 0.0),
+                     onCellClicked: String => Unit // Aggiornato a Coordinate => Unit per essere type-safe
+                   ) extends StackPane:
 
   private val hexagon: Polygon = Polygon()
-
-  hexagon.getPoints.addAll(
-    0.0, -20.2, 17.5, -10.1, 17.5, 10.1, 0.0, 20.2, -17.5, 10.1, -17.5, -10.1
-  )
-
-  hexagon.fill = Color.LightGrey
-  hexagon.stroke = Color.Black
-  hexagon.strokeWidth = 3.0
-  hexagon.rotate = 90.0
-  relocate(pos._1, pos._2)
-
   private val label = new Text()
-  label.text = s"${coordinate.x}, ${coordinate.y}"
-  label.fill = Color.Black
-  label.font = Font.font("Arial", 12)
-  label.mouseTransparent = true
+  children.addAll(hexagon, label)
+
+
+  private def setHexagon(): Unit =
+    hexagon.getPoints.addAll(
+      0.0, -20.2, 17.5, -10.1, 17.5, 10.1, 0.0, 20.2, -17.5, 10.1, -17.5, -10.1
+    )
+    hexagon.fill = Color.LightGrey
+    hexagon.stroke = Color.Black
+    hexagon.strokeWidth = 1.0
+    hexagon.rotate = 90.0
+    hexagon.setScaleX(1.2)
+    hexagon.setScaleY(1.2)
+    relocate(pos._1, pos._2)
+    label.text = s""
+    label.fill = Color.Black
+    label.font = Font.font("Arial", 12)
+    label.mouseTransparent = true
+    placeTokenView()
 
   hexagon.onMouseClicked = (event: MouseEvent) =>
     onCellClicked(
-      s"cliccato esagono con coordinata ${coordinate.x}, ${coordinate.y}"
+      "ciao" // Passa direttamente l'oggetto Coordinate tipato
+      //triggera la chiamata a Controller che avvisa Model che deve distruggere e ricostruire nuovo mondo
     )
 
   def updateState(newCell: Cell): Unit =
     println(
       s"Aggiornamento cella $coordinate nel view con i nuovi dati del modello"
+      //dopo aver costruito nuovo mondo faccio update linkando questa CellView a nuova Cell
+      //con stesse Coordinate ma List[Token] AGGIORNATA
     )
+    this.cell = newCell
+    placeTokenView()
 
-  def cellIsEligible(): Unit =
-    hexagon.stroke = Color.Green
+  def highlight(enabled: Boolean): Unit =
+    if enabled then
+      hexagon.fill = Color.LightGreen
+    else
+      hexagon.fill = Color.LightGrey
 
-  def revertBack(): Unit =
-    hexagon.stroke = Color.Black
+  private def placeTokenView(): Unit =
+    val tokens = cell.getTokens
+    val offsets = cell.height match
+      case 1 => List(0.0)
+      case 2 => List(3.0, -4.0)
+      case 3 => List(7.0, 0.0, -7.0)
+      case _ => Nil
+    tokens.zip(offsets).foreach { (token, offsetY) =>
+      val tv = createConfiguredTokenView(token, offsetY)
+      children.add(tv)
+    }
 
-  children.addAll(hexagon, label)
+  private def createConfiguredTokenView(token: TerrainToken, offsetY: Double): TokenView =
+    val tv = TokenView(token)
+    tv.setScaleX(0.8)
+    tv.setScaleY(0.8)
+    tv.setTranslateY(offsetY)
+    tv
+
+  setHexagon()
--- src/test/scala/it/unibo/view/cell/CellViewDemo.scala
+++ src/test/scala/it/unibo/view/cell/CellViewDemo.scala
@@ -1,17 +1,25 @@
 package it.unibo.view.cell
 
+
 import it.unibo.model.cell.Cell
 import it.unibo.model.personalBoard.Coordinate
+import it.unibo.model.token.TerrainToken.*
+import it.unibo.view.token.TokenView
 import scalafx.scene.Scene
 import scalafx.application.JFXApp3
-import scalafx.scene.layout.StackPane
+import scalafx.scene.layout.{Pane, StackPane}
 
 object CellViewDemo extends JFXApp3:
 
   override def start(): Unit =
     val f = (a: String) => println(a)
-    val root: StackPane =
-      CellView(Coordinate(0, 0), Cell(), (20.0, 50.0), f)
+    val c = Cell()
+    val c1 = c.placeToken(Ground)
+    val c2 = c1.placeToken(Ground)
+    val c3 = c2.placeToken(Forest)
+    val cv = CellView(Coordinate(0, 0), c3, (20.0, 50.0), f)
+    val root: StackPane = StackPane()
+    root.children.add(cv)
     stage = new JFXApp3.PrimaryStage:
       title = "Simple Hexagon"
       scene = new Scene(root, 400, 400)
