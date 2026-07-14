da7c6c76dce80eea15b2d89df911db2f3e5bb390
Elisa Yan <elisa.yan@studio.unibo.it>
2026-07-14 22:17:58 +0200

    refactor main

--- src/main/scala/it/unibo/Main.scala
+++ src/main/scala/it/unibo/Main.scala
@@ -2,7 +2,6 @@ package it.unibo
 
 import it.unibo.controller.GameController
 import it.unibo.model._
-import it.unibo.model.*
 import it.unibo.model.personalboard.BoardSide.SideA
 import it.unibo.model.personalboard.PersonalBoard
 import it.unibo.view.GameView
@@ -14,12 +13,22 @@ object Main extends JFXApp3:
   override def start(): Unit =
 
     val players =
-      List(Player(1, PersonalBoard(SideA)), Player(2, PersonalBoard(SideA)))
+      List(
+        Player(1, "Player1", PersonalBoard(SideA)),
+        Player(2, "Player2", PersonalBoard(SideA))
+      )
+
     var view: GameView = null
-    val controller = GameController(GameModel(players), () => view.refresh())
+    val controller = GameController(
+      GameModel(players),
+      (newModel, logMessage) =>
+        view.refresh(newModel.currentPlayer.name, logMessage)
+    )
 
     view = GameView(controller)
-    view.refresh()
+
+    controller.startGame()
+
     stage = new JFXApp3.PrimaryStage:
       title = "Scalarmonies"
       scene = new Scene:
