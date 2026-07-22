0506ae668903c42fcde2826b9f09547ee87e252a
Filoferro03 <filippo.ferretti03@gmail.com>
2026-07-22 22:43:44 +0200

    first simple implementation of home view

---
+++ src/test/scala/it/unibo/view/homepage/HomeViewDemo.scala
@@ -0,0 +1,17 @@
+package it.unibo.view.homepage
+
+import scalafx.application.JFXApp3
+import scalafx.scene.Scene
+
+object HomeViewDemo extends JFXApp3:
+
+  override def start(): Unit =
+    stage = new JFXApp3.PrimaryStage:
+      title = "Scalarmonies - Setup"
+      width = 500
+      height = 600
+      scene = new Scene:
+        root = HomeView: (playerNames, selectedSide) =>
+          println(s"Game Started!")
+          println(s"Players (${playerNames.size}): ${playerNames.mkString(", ")}")
+          println(s"Chosen Side: $selectedSide")
