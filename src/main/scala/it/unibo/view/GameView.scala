ebb090c4aad253ee63e8cbe73b32221780dcbe92
Elisa Yan <elisa.yan@studio.unibo.it>
2026-07-23 17:50:11 +0200

    integrate gameview with central board

--- src/main/scala/it/unibo/view/GameView.scala
+++ src/main/scala/it/unibo/view/GameView.scala
@@ -1,30 +1,43 @@
 package it.unibo.view
 
 import it.unibo.controller.GameController
-import it.unibo.model.token.TerrainToken
 import it.unibo.view.centralboard.CentralBoardView
 import it.unibo.view.infopanel.InfoPanelView
 import it.unibo.view.personalboard.PersonalBoardView
 import it.unibo.view.playerarea.PlayerAreaView
 import it.unibo.view.token.TokenView
-import scalafx.geometry.{Insets, Pos}
+import scalafx.geometry.Insets
+import scalafx.geometry.Pos
 import scalafx.scene.control.ScrollPane
-import scalafx.scene.layout.{Background, BackgroundFill, Border, BorderPane, BorderStroke, ColumnConstraints, CornerRadii, FlowPane, GridPane, HBox, RowConstraints, VBox}
+import scalafx.scene.layout.Background
+import scalafx.scene.layout.BackgroundFill
+import scalafx.scene.layout.ColumnConstraints
+import scalafx.scene.layout.CornerRadii
+import scalafx.scene.layout.FlowPane
+import scalafx.scene.layout.GridPane
+import scalafx.scene.layout.HBox
+import scalafx.scene.layout.RowConstraints
+import scalafx.scene.layout.VBox
 import scalafx.scene.paint.Color
-import scalafx.scene.layout.LayoutIncludes.jfxBorderStrokeStyle2sfx
-import scalafx.scene.text.{Font, FontWeight, Text}
+import scalafx.scene.text.Font
+import scalafx.scene.text.FontWeight
+import scalafx.scene.text.Text
 
 class GameView(controller: GameController) extends GridPane:
 
   private val systemMessageBar: HBox = new HBox():
     alignment = Pos.Center
     padding = Insets(10)
-    background = new Background(Array(new BackgroundFill(Color.Beige, new CornerRadii(5), Insets.Empty)))
+    background = new Background(
+      Array(new BackgroundFill(Color.Beige, new CornerRadii(5), Insets.Empty))
+    )
 
   private val commonMarketBar: HBox = new HBox():
     alignment = Pos.Center
     padding = Insets(15)
-    background = new Background(Array(new BackgroundFill(Color.Wheat, new CornerRadii(5), Insets.Empty)))
+    background = new Background(
+      Array(new BackgroundFill(Color.Wheat, new CornerRadii(5), Insets.Empty))
+    )
 
   private val playersContainer: FlowPane = new FlowPane():
     hgap = 30.0
@@ -42,41 +55,42 @@ class GameView(controller: GameController) extends GridPane:
     spacing = 20.0
     padding = Insets(1)
     alignment = Pos.CenterLeft
-    background = new Background(Array(new BackgroundFill(Color.LightGrey, new CornerRadii(5), Insets.Empty)))
+    background = new Background(
+      Array(
+        new BackgroundFill(Color.LightGrey, new CornerRadii(5), Insets.Empty)
+      )
+    )
 
   private val infoPanelLog: VBox = new VBox():
     spacing = 10.0
     padding = Insets(15)
     alignment = Pos.TopLeft
-    background = new Background(Array(new BackgroundFill(Color.Lavender, new CornerRadii(5), Insets.Empty)))
+    background = new Background(
+      Array(
+        new BackgroundFill(Color.Lavender, new CornerRadii(5), Insets.Empty)
+      )
+    )
 
   private def initLayout(): Unit =
     padding = Insets(10)
     hgap = 5.0
     vgap = 5.0
 
-    val colGameplay = new ColumnConstraints() {
+    val colGameplay = new ColumnConstraints():
       percentWidth = 84.0
-    }
-    val colTokenHand = new ColumnConstraints() {
+    val colTokenHand = new ColumnConstraints():
       percentWidth = 3.0
-    }
-    val colInfoPanel = new ColumnConstraints() {
+    val colInfoPanel = new ColumnConstraints():
       percentWidth = 13.0
-    }
     columnConstraints.addAll(colGameplay, colTokenHand, colInfoPanel)
 
-    val rowSystemMessage = new RowConstraints() {
+    val rowSystemMessage = new RowConstraints():
       percentHeight = 3.0
-    }
-    val rowCommonMarket = new RowConstraints() {
+    val rowCommonMarket = new RowConstraints():
       percentHeight = 32.0
-    }
-    val rowPlayersZone = new RowConstraints() {
+    val rowPlayersZone = new RowConstraints():
       percentHeight = 65.0
-    }
     rowConstraints.addAll(rowSystemMessage, rowCommonMarket, rowPlayersZone)
-
 
     add(systemMessageBar, 0, 0)
     add(commonMarketBar, 0, 1)
@@ -99,7 +113,8 @@ class GameView(controller: GameController) extends GridPane:
     tokens.foreach(token =>
       token.setScaleX(0.8)
       token.setScaleY(0.8)
-      personalTokenSidebar.children.add(token))
+      personalTokenSidebar.children.add(token)
+    )
 
   def updateInfoPanelLog(panel: InfoPanelView): Unit =
     infoPanelLog.children.clear()
@@ -113,9 +128,8 @@ class GameView(controller: GameController) extends GridPane:
     playersContainer.children.clear()
     areas.foreach(area => playersContainer.children.add(area))
 
-  def updateCentralBoard(board: CentralBoardView): Unit = {
+  def updateCentralBoard(board: CentralBoardView): Unit =
     commonMarketBar.children.clear()
     commonMarketBar.children.add(board)
-  }
 
   initLayout()
--- src/test/scala/it/unibo/view/GameViewDemo.scala
+++ src/test/scala/it/unibo/view/GameViewDemo.scala
@@ -3,11 +3,14 @@ package it.unibo.view
 import it.unibo.controller.GameController
 import it.unibo.model.GameModel
 import it.unibo.model.card.{AnimalCard, CellRequirement, Habitat}
+import it.unibo.model.centralboard.CentralBoards.CentralBoard
 import it.unibo.model.personalboard.BoardSide.SideA
 import it.unibo.model.personalboard.{Coordinate, PersonalBoard}
+import it.unibo.model.pouch.Pouches.Pouch
 import it.unibo.model.token.TerrainToken
 import it.unibo.model.token.TerrainToken.{Building, Field, Water}
 import it.unibo.view.card.AnimalCardView
+import it.unibo.view.centralboard.CentralBoardView
 import it.unibo.view.infopanel.InfoPanelView
 import it.unibo.view.personalboard.PersonalBoardView
 import it.unibo.view.playerarea.PlayerAreaView
@@ -35,38 +38,90 @@ object GameViewDemo extends JFXApp3:
       infoPanel.addEntry("Player 1", "Player 1 fakes drawing a card")
 
       // cards habitat
-      val forestHabitat = Habitat(List(
-        CellRequirement(Coordinate(0, 0), TerrainToken.Forest, 1),
-        CellRequirement(Coordinate(1, 1), TerrainToken.Field, 2)
-      ))
-      val waterHabitat = Habitat(List(
-        CellRequirement(Coordinate(0, 0), TerrainToken.Water, 1),
-        CellRequirement(Coordinate(-1, 1), TerrainToken.Mountain, 2)
-      ))
+      val forestHabitat = Habitat(
+        List(
+          CellRequirement(Coordinate(0, 0), TerrainToken.Forest, 1),
+          CellRequirement(Coordinate(1, 1), TerrainToken.Field, 2)
+        )
+      )
+      val waterHabitat = Habitat(
+        List(
+          CellRequirement(Coordinate(0, 0), TerrainToken.Water, 1),
+          CellRequirement(Coordinate(-1, 1), TerrainToken.Mountain, 2)
+        )
+      )
 
       val animalCards: List[AnimalCard] = List(
-        AnimalCard("Orso",  forestHabitat, List(4, 7, 12, 16), imageId = "default.png"),
-        AnimalCard("Lontra", waterHabitat, List(3, 6, 10, 15), imageId = "default.png"),
-        AnimalCard("Cervo", forestHabitat, List(3, 7, 11, 16), imageId = "default.png")
+        AnimalCard(
+          "Orso",
+          forestHabitat,
+          List(4, 7, 12, 16),
+          imageId = "default.png"
+        ),
+        AnimalCard(
+          "Lontra",
+          waterHabitat,
+          List(3, 6, 10, 15),
+          imageId = "default.png"
+        ),
+        AnimalCard(
+          "Cervo",
+          forestHabitat,
+          List(3, 7, 11, 16),
+          imageId = "default.png"
+        ),
+        AnimalCard(
+          "Volpe",
+          forestHabitat,
+          List(5, 8, 12, 18),
+          imageId = "default.png"
+        ),
+        AnimalCard(
+          "Gufo",
+          waterHabitat,
+          List(4, 9, 14, 20),
+          imageId = "default.png"
+        )
       )
+
+      // central board
+      val pouch: Pouch = Pouch.initialPouch()
+      val (centralBoard, _, _) =
+        CentralBoard.empty.fill(
+          pouch = pouch,
+          deck = animalCards
+        )
+
+      val centralBoardView =
+        CentralBoardView(
+          board = centralBoard,
+          onCardClicked = controller.onTakeAnimalCard,
+          onTokenClicked = controller.onTakeTokens
+        )
 
       // player 1
       val board1 = PersonalBoard(SideA)
       val boardView1 = PersonalBoardView(board1, controller)
-      val availableCards1: List[Node] = animalCards.take(2).map(c => AnimalCardView(c, 90, 140))
-      val completedCards1: List[Node] = animalCards.drop(2).map(c => AnimalCardView(c, 55, 85))
-      val playerArea1 = PlayerAreaView(boardView1, availableCards1, completedCards1, "Player 1")
+      val availableCards1: List[Node] =
+        animalCards.take(2).map(c => AnimalCardView(c, 90, 140))
+      val completedCards1: List[Node] =
+        animalCards.drop(2).map(c => AnimalCardView(c, 55, 85))
+      val playerArea1 =
+        PlayerAreaView(boardView1, availableCards1, completedCards1, "Player 1")
 
       // player 2
       val board2 = PersonalBoard(SideA)
       val boardView2 = PersonalBoardView(board2, controller)
-      val availableCards2: List[Node] = animalCards.take(1).map(c => AnimalCardView(c, 90, 140))
+      val availableCards2: List[Node] =
+        animalCards.take(1).map(c => AnimalCardView(c, 90, 140))
       val completedCards2: List[Nothing] = List.empty
-      val playerArea2 = PlayerAreaView(boardView2, availableCards2, completedCards2, "Player 2")
+      val playerArea2 =
+        PlayerAreaView(boardView2, availableCards2, completedCards2, "Player 2")
 
       root.updateSystemMessageBar("Turno di: Player 1")
       root.updatePersonalTokenSidebar(tokens)
       root.updateInfoPanelLog(infoPanel)
+      root.updateCentralBoard(centralBoardView)
       root.updatePlayerAreas(List(playerArea1, playerArea2))
 
       scene = new Scene(root)
