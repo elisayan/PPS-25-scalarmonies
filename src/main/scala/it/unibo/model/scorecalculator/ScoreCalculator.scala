c1a1a855efb34519a2450b0320b7fc517a7171d3
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2026-07-28 21:35:05 +0200

    modified score calculator to obj singleton

--- src/main/scala/it/unibo/model/scorecalculator/ScoreCalculator.scala
+++ src/main/scala/it/unibo/model/scorecalculator/ScoreCalculator.scala
@@ -5,76 +5,61 @@ import it.unibo.model.personalboard.Coordinate
 import it.unibo.model.personalboard.PersonalBoard
 import it.unibo.model.scorecalculator.Score.Score
 
-trait ScoreCalculator:
+object ScoreCalculator:
 
   def calculateScore(
       personalBoard: PersonalBoard,
       cards: List[AnimalCard]
-  ): Score
+  ): Score =
+    scoreFromFields(personalBoard) +
+      scoreFromMountains(personalBoard) +
+      scoreFromBuildings(personalBoard) +
+      scoreFromForests(personalBoard) +
+      scoreFromWater(personalBoard) +
+      scoreFromAnimalCards(cards) +
+      scoreFromSpirits(personalBoard)
 
   def calculateDetailedScore(
       board: PersonalBoard,
       cards: List[AnimalCard]
-  ): (Score, Map[String, Score])
+  ): (Score, Map[String, Score]) =
+    val map: Map[String, Score] = Map(
+      "Field" -> scoreFromFields(board),
+      "Water" -> scoreFromWater(board),
+      "Building" -> scoreFromBuildings(board),
+      "Forest" -> scoreFromForests(board),
+      "Mountain" -> scoreFromMountains(board),
+      "Animal Cards" -> scoreFromAnimalCards(cards)
+    )
 
-object ScoreCalculator:
-  def apply(): ScoreCalculator = ScoreCalculatorImpl()
+    (calculateScore(board, cards), map)
 
-  private case class ScoreCalculatorImpl() extends ScoreCalculator:
+  @scala.annotation.tailrec
+  private def buildGroup(
+      group: Set[Coordinate],
+      remaining: Set[Coordinate]
+  ): (Set[Coordinate], Set[Coordinate]) =
+    val (connected, farAway) =
+      remaining.partition(coord => coord.allNeighbours.exists(group.contains))
+    if connected.isEmpty then (group, farAway)
+    else buildGroup(group ++ connected, farAway)
 
-    @scala.annotation.tailrec
-    private def buildGroup(
-        group: Set[Coordinate],
-        remaining: Set[Coordinate]
-    ): (Set[Coordinate], Set[Coordinate]) =
-      val (connected, farAway) =
-        remaining.partition(coord => coord.allNeighbours.exists(group.contains))
-      if connected.isEmpty then (group, farAway)
-      else buildGroup(group ++ connected, farAway)
+  private def scoreFromFields(board: PersonalBoard): Score =
+    FieldsScoring.compute(board, buildGroup)
 
-    override def calculateScore(
-        board: PersonalBoard,
-        cards: List[AnimalCard]
-    ): Score =
-      scoreFromFields(board) +
-        scoreFromMountains(board) +
-        scoreFromBuildings(board) +
-        scoreFromForests(board) +
-        scoreFromWater(board) +
-        scoreFromAnimalCards(cards) +
-        scoreFromSpirits(board)
+  private def scoreFromMountains(board: PersonalBoard): Score =
+    MountainsScoring.compute(board, buildGroup)
 
-    private def scoreFromFields(board: PersonalBoard): Score =
-      FieldsScoring.compute(board, buildGroup)
+  private def scoreFromBuildings(board: PersonalBoard): Score =
+    BuildingsScoring.compute(board)
 
-    private def scoreFromMountains(board: PersonalBoard): Score =
-      MountainsScoring.compute(board, buildGroup)
+  private def scoreFromForests(board: PersonalBoard): Score =
+    ForestsScoring.compute(board)
 
-    private def scoreFromBuildings(board: PersonalBoard): Score =
-      BuildingsScoring.compute(board)
+  private def scoreFromWater(board: PersonalBoard): Score =
+    WaterScoring.compute(board, buildGroup)
 
-    private def scoreFromForests(board: PersonalBoard): Score =
-      ForestsScoring.compute(board)
+  private def scoreFromAnimalCards(cards: List[AnimalCard]): Score =
+    Score(cards.foldLeft(0)((current, card) => current + card.currentPoints))
 
-    private def scoreFromWater(board: PersonalBoard): Score =
-      WaterScoring.compute(board, buildGroup)
-
-    private def scoreFromAnimalCards(cards: List[AnimalCard]): Score =
-      Score(cards.foldLeft(0)((current, card) => current + card.currentPoints))
-
-    private def scoreFromSpirits(board: PersonalBoard): Score = Score.zero
-
-    override def calculateDetailedScore(
-        board: PersonalBoard,
-        cards: List[AnimalCard]
-    ): (Score, Map[String, Score]) =
-      val map: Map[String, Score] = Map(
-        "Field" -> scoreFromFields(board),
-        "Water" -> scoreFromWater(board),
-        "Building" -> scoreFromBuildings(board),
-        "Forest" -> scoreFromForests(board),
-        "Mountain" -> scoreFromMountains(board),
-        "Animal Cards" -> scoreFromAnimalCards(cards)
-      )
-
-      (calculateScore(board, cards), map)
+  private def scoreFromSpirits(board: PersonalBoard): Score = Score.zero
