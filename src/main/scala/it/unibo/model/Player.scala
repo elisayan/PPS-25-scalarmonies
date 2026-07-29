916fa91b0553d809abf850b232c75ba03f68e92f
Elisa Yan <elisa.yan@studio.unibo.it>
2026-07-29 23:52:25 +0200

    added scaladocs for player

--- src/main/scala/it/unibo/model/Player.scala
+++ src/main/scala/it/unibo/model/Player.scala
@@ -3,6 +3,18 @@ package it.unibo.model
 import it.unibo.model.card.AnimalCard
 import it.unibo.model.personalboard.PersonalBoard
 
+/** Represents a player participating in the game.
+ * @param id
+ *  the player's unique identifier.
+ * @param name
+ *  the player's name.
+ * @param board
+ *  the player's personal board.
+ * @param activeCards
+ *  the animal cards currently owned by the player.
+ * @param completedCards
+ *  the completed animal cards.
+ */
 case class Player(
     id: Int,
     name: String,
