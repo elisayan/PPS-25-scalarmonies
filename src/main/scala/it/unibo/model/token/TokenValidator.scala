4fc16c5ace66e6a99703666fbc294794fe3bc5a2
Elisa Yan <elisa.yan@studio.unibo.it>
2026-07-29 23:48:47 +0200

    added scaladocs for token

--- src/main/scala/it/unibo/model/token/TokenValidator.scala
+++ src/main/scala/it/unibo/model/token/TokenValidator.scala
@@ -5,10 +5,19 @@ import it.unibo.model.personalboard.Coordinate
 import it.unibo.model.personalboard.PersonalBoard
 import it.unibo.model.token.TerrainToken._
 
+/** Validates terrain token placement according to the game rules. */
 object TokenValidator:
 
   private val MaxStackHeight = 3
 
+  /** Checks whether a terrain token can be placed on a cell.
+   * @param token
+   *  the terrain token to place.
+   * @param cell
+   *  the target cell.
+   * @return
+   *  true if the placement is valid, false otherwise.
+   */
   def canPlace(token: TerrainToken, cell: Cell): Boolean =
     val height = cell.getTokens.length
     if height >= MaxStackHeight then false
@@ -26,6 +35,14 @@ object TokenValidator:
             case _                            => false
           }
 
+  /** Returns all valid placement coordinates for a terrain token.
+   * @param token
+   *  the terrain token to place.
+   * @param board
+   *  the player's board.
+   * @return
+   *  the list of valid coordinates.
+   */
   def validPositions(
       token: TerrainToken,
       board: PersonalBoard
